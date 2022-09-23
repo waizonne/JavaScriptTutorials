334aa5818f39058b634bbf5da0c30c3c9a28523a
waizonne <abhisit@ukham.com>
2022-09-23 22:52:33 +0700

    Add files via upload

---
+++ ex4/js_ex4_2.js
@@ -0,0 +1,4 @@
+//Ex4.2: JavaScript Numbers and Strings
+document.getElementById("ex4-2-1").innerHTML = 93.69; //Number
+document.getElementById("ex4-2-2").innerHTML = "Abhisit"; //String
+document.getElementById("ex4-2-3").innerHTML = "Abhisit score: " + 93.69; //Number + String
